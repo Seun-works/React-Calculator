6d65900c3f0dddd70c487885a0a491fca0cc7c54
SeunOgundipe <Sogundipe@gmail.com>
2022-01-05 12:30:35 +0100

    Defined the styling of the calculator and its buttons

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './Styles.css';
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">123324</div>
+        <div className="current-operand">114678</div>
+      </div>
+      <button className = "span-two">AC</button>
+      <button className = "">DEL</button>
+      <button className = "">÷</button>
+      <button className = "">1</button>
+      <button className = "">2</button>
+      <button className = "">3</button>
+      <button className = "">*</button>
+      <button className = "">4</button>
+      <button className = "">5</button>
+      <button className = "">6</button>
+      <button className = "">+</button>
+      <button className = "">7</button>
+      <button className = "">8</button>
+      <button className = "">9</button>
+      <button className = "">-</button>
+      <button className = "">.</button>
+      <button className = "">0</button>
+      <button className = "span-two">=</button>
+      
+    </div>
+  );
+}
+
+export default App;
