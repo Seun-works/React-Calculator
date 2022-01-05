72ebdafce5656ed5a3f04604acca3abd5b80716d
SeunOgundipe <Sogundipe@gmail.com>
2022-01-05 13:18:50 +0100

    added functionality for adding digits to the calculator

---
+++ src/DigitButton.jsx
@@ -0,0 +1,10 @@
+import { ACTIONS } from './App';
+import React from 'react'
+
+const DigitButton = ({dispatch, digit}) => {
+    return (
+        <button onClick = {() => dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit}})}>{digit}</button>
+    )
+}
+
+export default DigitButton;
