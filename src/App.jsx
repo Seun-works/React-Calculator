95d7f9e2945f11d6cc373d77d3f8c17d36c9bed3
SeunOgundipe <Sogundipe@gmail.com>
2022-01-06 11:32:57 +0100

    added the delete functionality to the calculator

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,14 @@ export const ACTIONS = {
 const reducer = (state, {type, payload}) => {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if(state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false
+        }
+      }
+      
       if(payload.digit === '0' && state.currentOperand === '0') {
         return state}
       if(payload.digit === '.' && state.currentOperand.includes(".")) {
@@ -26,10 +34,35 @@ const reducer = (state, {type, payload}) => {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       }
+      
     case ACTIONS.CLEAR: 
      return {
        
      }  
+
+    case ACTIONS.DELETE_DIGIT:
+      if(state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null
+        }
+      }
+      if(state.currentOperand == null) {
+        return state
+      }
+      if(state.currentOperand.length === 1 ) {
+        return {
+          ...state,
+          currentOperand: null
+        }
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+
+      }
+
      case ACTIONS.CHOOSE_OPERATION:
        if(state.currentOperand == null && state.previousOperand == null) {
          return state
@@ -63,6 +96,7 @@ const reducer = (state, {type, payload}) => {
        }
        return {
          ...state,
+         overwrite: true,
          previousOperand: null,
          currentOperand: evaluate(state),
          operation: null
@@ -109,7 +143,7 @@ function App() {
         <div className="current-operand">{currentOperand}</div>
       </div>
       <button className = "span-two" onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
-      <button className = "">DEL</button>
+      <button className = "" onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}>DEL</button>
       <OperationButton operation = "÷" dispatch={dispatch} />
       <DigitButton digit = "1" dispatch={dispatch} />
       <DigitButton digit = "2" dispatch={dispatch} />
